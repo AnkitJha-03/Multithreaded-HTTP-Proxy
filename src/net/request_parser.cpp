485d7b89d5f862f8d990363cea0831257cc86059
Ankit Jha <ankitjhax03@gmail.com>
2025-06-26 23:49:24 +0530

    feat: added blocklist feature to block a domains

--- src/net/request_parser.cpp
+++ src/net/request_parser.cpp
@@ -1,11 +1,16 @@
 #include "request_parser.hpp"
+#include "blocklist.hpp"
 #include "cache.hpp"
+#include <algorithm>
 #include <cstring>
 #include <mutex>
 
 // Global cache
 LRUCache response_cache(100);
 std::mutex cache_mutex;
+
+// Global Blocklist
+Blocklist blocklist;
 
 std::string request_parser(const char request[]) {
   // Check if the request is for the root path ("/")
@@ -30,14 +35,22 @@ std::string request_parser(const char request[]) {
   std::string url(path_start, path_end - path_start);
 
   // Normalize URL by removing http:// or https:// prefix
-  if (url.rfind("http://", 0) == 0) {
+  if (url.find("http://", 0) == 0) {
     url = url.substr(7);  // remove "http://"
-  } else if (url.rfind("https://", 0) == 0) {
+  } else if (url.find("https://", 0) == 0) {
     url = url.substr(8);  // remove "https://"
   }
   // Remove trailing slash
   if (!url.empty() && url.back() == '/') {
     url.pop_back();
+  }
+  // Convert URL to lowercase for case-insensitive comparison
+  std::transform(url.begin(), url.end(), url.begin(), ::tolower);
+
+  // Check if the URL is blocked
+  if (blocklist.is_blocked(url)) {
+    return "HTTP/1.1 403 Forbidden\r\n"
+           "\r\n";
   }
 
   // Check cache first
